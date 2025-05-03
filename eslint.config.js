622520e6938e3805c9815ebee9194cc9bf6b50ac
Vladykkk <rubakvlad7@gmail.com>
2025-05-03 11:34:22 +0300

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  eslintConfigPrettier,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      quotes: ["error", "double"],
+    },
+  },
+]);
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <section>
+      <h1>Brilliant Parallax</h1>
+    </section>
+  );
+}
+
+export default App;
